e9ee66a4dcad96c27a4927c18aad1c1847d81e30
vanshchapadia17 <chapadiav01@gmail.com>
2026-03-27 11:29:39 +0530

    symple chtbot using hugging face

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,26 @@
-from langgraph.graph import StarGraph,START,END
+import os
+from dotenv import load_dotenv
+
+from langgraph.graph import StateGraph,START,END, StateGraph
 from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage, HumanMessage
 from typing import TypedDict,Annotated
-from langchain_groq import Groq
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
 
-llm = Groq()
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1",  # or any free model
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.5,
+    do_sample=False,
+    repetition_penalty=1.03,
+    provider="novita",
+    huggingfacehub_api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
+
+chat_model = ChatHuggingFace(llm=llm,verbose=True) 
 
 class ChatState(TypedDict):
     
@@ -15,18 +31,29 @@ def chat_node(state:ChatState):
     messages=state['messages']
 
     #generate response using llm
-    response = llm.invoke(messages)
+    response = chat_model.invoke(messages)
 
     #response to store
     return {'messages': [response]}
 
 
 
-graph = StarGraph(ChatState)
-
+graph = StateGraph(ChatState)
 graph.add_node('chat_node',chat_node)
-
 graph.add_edge(START,'chat_node')
 graph.add_edge('chat_node',END)
 
 chatbot = graph.compile()
+
+while True:
+    user_input = input("User: ")
+
+    if user_input.strip() in ['exit', 'quit','bye']:
+        print("Exiting chatbot.")
+        break
+
+    # Run the chatbot with the user's message
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]})
+
+    # Print the chatbot's response
+    print('AI:', response['messages'][-1].content)
